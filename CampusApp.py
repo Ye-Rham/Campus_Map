cda40eaedb0e643c6e39a9593f1321f35d31db85
Ye-Rham <43466234+Ye-Rham@users.noreply.github.com>
2019-04-15 14:32:13 -0700

    Revamp

--- CampusApp.py
+++ CampusApp.py
@@ -3,85 +3,87 @@ from tkinter import *
 from PIL import ImageTk, Image
 import os
 
+
 screen = tk.Tk()
+floors_file = open("Floors.txt", "r")
+floors = []
+for line in floors_file:
+	floors.append(line.rstrip())
+floors_file.close()
 
 
 def search():
-	floors_file = open("Floors.txt", "r")
-	flag = 0
-	start = 0
-	searched = str(entry_box.get())
-	last_position = floors_file.tell()
-	
-	# print (floors_file.readline())
-	"""for line in floors_file.readline():
-		print ("test 1")
-		print (line)
-		for word in line.split():
-			print ("test 2")
-			print (word)
-			if word == searched:
-				print ("test 3")
-				flag = 1
-				while word != ',':
-	
-	
-				print (line)"""
-	if "100" > "101":
-		print("true")
-	
-	if searched == '':
-		pass
-	
-	elif (searched >= "0" and searched < "4000000000000") or searched < "0":
-		pass
-	
-	elif searched in floors_file.read():
-		
-		floors_file.seek(last_position)		
-		
-		for line in floors_file.readlines():
-			
-			if searched in line:			
-				start = 1
+	flag = False
 
-			if start == 0:
-				pass
-			else:
-				if "," in line:				
-					break
-				else:
-					print (line)
-		flag = 1
-	
-	if flag == 0:
-		not_valid = tk.Label(screen, text="Not A Valid Input, Search Again")
-		not_valid.place(x=625, y=25)
-		print("Not a valid input")
-	
-	floors_file.close()
+	if entry_box.get() is not "":
+		for x in range(0, len(floors)):
+			if selection.get() == floors[x].split('	', 1)[0]:
+				while floors[x] != "," and flag is not True:
+					x += 1
+					if entry_box.get() == floors[x]:
+						flag = True
+						not_valid.place_forget()
+						while len(floors[x].split(' ', 2)) != 2:
+							x -= 1
+						print(floors[x].replace(':', ''))
+
+		if not flag:
+			not_valid.place(x=645, y=25)
+
+	if selection.get() is "E":
+		print("")
+	elif selection.get() is "CS":
+		print("")
+	elif selection.get() is "EC":
+		print("")
+	elif selection.get() is "GH":
+		print("")
+	elif selection.get() is "Hum":
+		print("")
+	elif selection.get() is "LH":
+		print("")
+	elif selection.get() is "MH":
+		print("")
 
 
 # screen title and size
 screen.title("Campus")
 screen.geometry("930x610")
 
+# Campus image
+img = ImageTk.PhotoImage(Image.open("campus.png"))
+panel = tk.Label(screen, image=img)
+panel.pack(side=BOTTOM)
+
 # What can be searched for
 title_question = tk.Label(screen, text="Classes to Search:")
-title_question.pack(side = TOP)
+title_question.pack(side=TOP)
 
 
 # Entry Box
-entry_box = tk.Entry(screen)
-entry_box.pack(side = TOP)
+Label(screen, text="Room #:").place(x=438, y=25)
+entry_box = tk.Entry(screen, width=6)
+entry_box.place(x=500, y=25)
 
 # Search Button
-search_button = tk.Button(screen, text="Search", bg="Blue",command=search)
-search_button.place(x = 550, y = 20)
+search_button = tk.Button(screen, text="Search", bg="Blue", command=search)
+search_button.place(x=558, y=20)
 
-img = ImageTk.PhotoImage(Image.open("campus.png"))
-panel = tk.Label(screen, image = img)
-panel.pack(side = BOTTOM)
+# Dropdown menu
+Label(screen, text="Building Initials:").place(x=252, y=25)
+buildings = ["E", "CS", "EC", "GH", "Hum", "LH", "MH"]
+selection = StringVar(screen)
+selection.set(buildings[0])
+buildings_dropdown = OptionMenu(screen, selection, *buildings)
+buildings_dropdown.config(width=3)
+buildings_dropdown.place(x=360, y=20)
 
+# Information box
+
+# Building highlight
+
+
+# Error text
+not_valid = tk.Label(screen, text="Invalid room #")
 
 screen.mainloop()
